26b03531c8ee94cc563d968a4ebb650677323a31
Eliana <valdezeliana38@gmail.com>
2025-08-31 04:17:20 -0300

    Crear el proyecto y diseño del formulario de inicio

---
+++ Inicio.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace quadratic_solver
+{
+    partial class Inicio
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label6 = new Label();
+            textBox3 = new TextBox();
+            label7 = new Label();
+            textBox4 = new TextBox();
+            label8 = new Label();
+            textBox5 = new TextBox();
+            label9 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.IndianRed;
+            pictureBox1.Location = new Point(-1, -3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1460, 99);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.IndianRed;
+            pictureBox2.Image = Properties.Resources.e_logo;
+            pictureBox2.Location = new Point(55, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(135, 96);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.IndianRed;
+            label1.Font = new Font("Dubai", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.GradientInactiveCaption;
+            label1.Location = new Point(989, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(370, 79);
+            label1.TabIndex = 2;
+            label1.Text = "Quadratic Solver";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.SeaShell;
+            label2.Font = new Font("Dubai", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(478, 120);
+            label2.Name = "label2";
+            label2.Size = new Size(438, 75);
+            label2.TabIndex = 3;
+            label2.Text = "Calculadora de Raices";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(64, 64, 64);
+            label3.Location = new Point(263, 180);
+            label3.Name = "label3";
+            label3.Size = new Size(839, 56);
+            label3.TabIndex = 4;
+            label3.Text = "Ingrese los valores de los coeficientes para la ecuación ax² + bx + c = 0 y el programa calculará \r\nlas raíces correspondientes.";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.IndianRed;
+            label4.Font = new Font("Calibri", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.SeaShell;
+            label4.Location = new Point(603, 260);
+            label4.Name = "label4";
+            label4.Size = new Size(143, 41);
+            label4.TabIndex = 5;
+            label4.Text = "ax²+bx+c";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.ForeColor = Color.IndianRed;
+            groupBox1.Location = new Point(227, 328);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(953, 105);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Valores";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox5);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox2.ForeColor = Color.IndianRed;
+            groupBox2.Location = new Point(227, 523);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(953, 105);
+            groupBox2.TabIndex = 7;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Raices";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.IndianRed;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.SeaShell;
+            button1.Location = new Point(603, 451);
+            button1.Name = "button1";
+            button1.Size = new Size(143, 44);
+            button1.TabIndex = 8;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.IndianRed;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.SeaShell;
+            button2.Location = new Point(478, 666);
+            button2.Name = "button2";
+            button2.Size = new Size(143, 44);
+            button2.TabIndex = 9;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.IndianRed;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.SeaShell;
+            button3.Location = new Point(773, 666);
+            button3.Name = "button3";
+            button3.Size = new Size(143, 44);
+            button3.TabIndex = 10;
+            button3.Text = "Salir";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Symbol", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Gray;
+            label5.Location = new Point(160, 46);
+            label5.Name = "label5";
+            label5.Size = new Size(92, 31);
+            label5.TabIndex = 0;
+            label5.Text = "Valor a";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.GradientInactiveCaption;
+            textBox1.Location = new Point(258, 46);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(73, 36);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.GradientInactiveCaption;
+            textBox2.Location = new Point(463, 46);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(73, 36);
+            textBox2.TabIndex = 3;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Symbol", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Gray;
+            label6.Location = new Point(363, 46);
+            label6.Name = "label6";
+            label6.Size = new Size(94, 31);
+            label6.TabIndex = 2;
+            label6.Text = "Valor b";
+            label6.Click += label6_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.GradientInactiveCaption;
+            textBox3.Location = new Point(671, 46);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(73, 36);
+            textBox3.TabIndex = 5;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Symbol", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Gray;
+            label7.Location = new Point(574, 46);
+            label7.Name = "label7";
+            label7.Size = new Size(91, 31);
+            label7.TabIndex = 4;
+            label7.Text = "Valor c";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.GradientInactiveCaption;
+            textBox4.Location = new Point(363, 46);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(73, 36);
+            textBox4.TabIndex = 7;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Symbol", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.Gray;
+            label8.Location = new Point(315, 44);
+            label8.Name = "label8";
+            label8.Size = new Size(42, 38);
+            label8.TabIndex = 6;
+            label8.Text = "x₁";
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = SystemColors.GradientInactiveCaption;
+            textBox5.Location = new Point(564, 46);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(73, 36);
+            textBox5.TabIndex = 9;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Symbol", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.Gray;
+            label9.Location = new Point(516, 44);
+            label9.Name = "label9";
+            label9.Size = new Size(42, 38);
+            label9.TabIndex = 8;
+            label9.Text = "x₂";
+            // 
+            // Inicio
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SeaShell;
+            ClientSize = new Size(1421, 774);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            ForeColor = Color.IndianRed;
+            Name = "Inicio";
+            Text = "Quadratic Solver";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label5;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox3;
+        private Label label7;
+        private TextBox textBox2;
+        private Label label6;
+        private TextBox textBox4;
+        private Label label8;
+        private TextBox textBox5;
+        private Label label9;
+    }
+}
---
+++ Inicio.cs
@@ -0,0 +1,15 @@
+namespace quadratic_solver
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
